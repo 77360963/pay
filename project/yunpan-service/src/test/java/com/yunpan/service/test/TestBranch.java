01b02db3e0ac57e8667633239b26be16eeb9d8a6
likunling <likunling@kjtpay.com.cn>
2018-11-28 19:06:49 +0800

    ceshi

--- project/yunpan-service/src/test/java/com/yunpan/service/test/TestBranch.java
+++ project/yunpan-service/src/test/java/com/yunpan/service/test/TestBranch.java
@@ -3,7 +3,7 @@ package com.yunpan.service.test;
 public class TestBranch {
     
     public static void main(String[] args) {
-        System.out.println("测试分支");
+        System.out.println("测试分支-----------测试");
     }
 
 }
